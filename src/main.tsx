589fc868f2186c57f018876d0a0d611ae62ec0de
Bhanu Prakash Bhanuri <bhanuprakashbhanuri@gmail.com>
2025-11-08 12:26:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
+// src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
+
