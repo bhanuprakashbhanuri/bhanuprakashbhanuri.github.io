d6aa2cc558c1663eda698e31b14ad46258041685
Bhanu Prakash Bhanuri <bhanuprakashbhanuri@gmail.com>
2025-11-08 13:29:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-// src/App.tsx
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
-
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import your pages/components
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => {
+const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class">
-        <TooltipProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* add extra routes here */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-
-          {/* global toasters (if you use them) */}
-          <Toaster />
-          <Sonner />
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* other routes */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
 
